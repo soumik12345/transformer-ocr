646ff1e38458f8e2459fae3b9837f1b6790fedc0
soumik12345 <19soumik.rakshit96@gmail.com>
2020-03-27 10:41:31 +0530

    Added Multi Head Attention:

--- src/blocks.py
+++ src/blocks.py
@@ -9,3 +9,34 @@ def scaled_dot_product_attention(q, k, v):
     output = tf.matmul(attention_weights, v)
     return output, attention_weights
 
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+
+    def __init__(self, d_model, n_heads):
+        super(MultiHeadAttention, self).__init__()
+        assert d_model % n_heads == 0
+        self.d_model = d_model
+        self.n_heads = n_heads
+        self.depth = self.d_model // self.n_heads
+        self.wq = tf.keras.layers.Dense(self.d_model)
+        self.wk = tf.keras.layers.Dense(self.d_model)
+        self.wv = tf.keras.layers.Dense(self.d_model)
+        self.dense = tf.keras.layers.Dense(self.d_model)
+
+    def split_heads(self, x, batch_size):
+        x = tf.reshape(x, (batch_size, -1, self.n_heads, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, v, k, q):
+        batch_size = tf.shape(q)[0]
+        q = self.wq(q)
+        k = self.wk(k)
+        v = self.wv(v)
+        q = self.split_heads(q, batch_size)
+        k = self.split_heads(k, batch_size)
+        v = self.split_heads(v, batch_size)
+        scaled_attention, attention_weights = scaled_dot_product_attention(q, k, v)
+        scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
+        concat_attention = tf.reshape(scaled_attention, (batch_size, -1, self.d_model))
+        output = self.dense(concat_attention)
+        return output, attention_weights
--- test.py
+++ test.py
@@ -5,11 +5,14 @@ from src.layers import *
 import tensorflow as tf
 from matplotlib import pyplot as plt
 
+
+# Test Feature Extractor
 # model = FeatureExtractor(weights=None)
 # x = tf.random.normal((1, 96, 96, 3))
 # y = model(x)
 # print(y.shape)
 
+# Test Positional Encoding
 # pos_encoding = positional_encoding(36, 256)
 # plt.pcolormesh(pos_encoding[0], cmap='RdBu')
 # plt.xlabel('Depth')
@@ -20,28 +23,36 @@ from matplotlib import pyplot as plt
 # plt.show()
 
 
-def test_scaled_dot_product_attention(q, k, v):
-    temp_out, temp_attn = scaled_dot_product_attention(q, k, v)
-    print('Attention weights are:')
-    print(temp_attn)
-    print('Output is:')
-    print(temp_out)
+# Test Scaled Dot Product Attention
+# def test_scaled_dot_product_attention(q, k, v):
+#     temp_out, temp_attn = scaled_dot_product_attention(q, k, v)
+#     print('Attention weights are:')
+#     print(temp_attn)
+#     print('Output is:')
+#     print(temp_out)
+#
+#
+# np.set_printoptions(suppress=True)
+# temp_k = tf.constant([[10, 0, 0],
+#                       [0, 10, 0],
+#                       [0, 0, 10],
+#                       [0, 0, 10]], dtype=tf.float32)
+# temp_v = tf.constant([[1, 0],
+#                       [10, 0],
+#                       [100, 5],
+#                       [1000, 6]], dtype=tf.float32)
+# temp_q = tf.constant([[0, 10, 0]], dtype=tf.float32)
+# test_scaled_dot_product_attention(temp_q, temp_k, temp_v)
+# temp_q = tf.constant([[0, 0, 10]], dtype=tf.float32)
+# test_scaled_dot_product_attention(temp_q, temp_k, temp_v)
+# temp_q = tf.constant([[10, 10, 0]], dtype=tf.float32)
+# test_scaled_dot_product_attention(temp_q, temp_k, temp_v)
+# temp_q = tf.constant([[0, 0, 10], [0, 10, 0], [10, 10, 0]], dtype=tf.float32)
+# test_scaled_dot_product_attention(temp_q, temp_k, temp_v)
 
 
-np.set_printoptions(suppress=True)
-temp_k = tf.constant([[10, 0, 0],
-                      [0, 10, 0],
-                      [0, 0, 10],
-                      [0, 0, 10]], dtype=tf.float32)
-temp_v = tf.constant([[1, 0],
-                      [10, 0],
-                      [100, 5],
-                      [1000, 6]], dtype=tf.float32)
-temp_q = tf.constant([[0, 10, 0]], dtype=tf.float32)
-test_scaled_dot_product_attention(temp_q, temp_k, temp_v)
-temp_q = tf.constant([[0, 0, 10]], dtype=tf.float32)
-test_scaled_dot_product_attention(temp_q, temp_k, temp_v)
-temp_q = tf.constant([[10, 10, 0]], dtype=tf.float32)
-test_scaled_dot_product_attention(temp_q, temp_k, temp_v)
-temp_q = tf.constant([[0, 0, 10], [0, 10, 0], [10, 10, 0]], dtype=tf.float32)
-test_scaled_dot_product_attention(temp_q, temp_k, temp_v)
+# Test Multi Head Attention
+temp_mha = MultiHeadAttention(d_model=512, n_heads=8)
+y = tf.random.uniform((1, 60, 512))  # (batch_size, encoder_sequence, d_model)
+out, attn = temp_mha(y, k=y, q=y)
+print(out.shape, attn.shape)

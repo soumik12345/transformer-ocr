c88ee9461d6c1a5952efa5fa025ae13546b8b4eb
soumik12345 <19soumik.rakshit96@gmail.com>
2020-03-29 00:31:08 +0530

    Added Blocks

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import numpy as np
 from src.utils import *
+from src.encoder import *
 from src.layers import *
 import tensorflow as tf
 from matplotlib import pyplot as plt
@@ -13,76 +14,23 @@ from matplotlib import pyplot as plt
 
 
 # Test Positional Encoding
+# pos_encoding = positional_encoding(36, 256)
+# print(pos_encoding.shape)
+# plt.pcolormesh(pos_encoding, cmap='RdBu')
+# plt.xlabel('Depth')
+# plt.xlim((0, 256))
+# plt.ylabel('Position')
+# plt.colorbar()
+# plt.title('Positional Encoding Test')
+# plt.show()
+
+
+feature_extractor = get_feature_extractor(weights=None)
+encoder = Encoder(36, 256, 4, 2)
+
+input_image = tf.random.normal((1, 96, 96, 3))
+features = FeatureExtractor(weights=None)(input_image)
 pos_encoding = positional_encoding(36, 256)
-print(pos_encoding.shape)
-plt.pcolormesh(pos_encoding, cmap='RdBu')
-plt.xlabel('Depth')
-plt.xlim((0, 256))
-plt.ylabel('Position')
-plt.colorbar()
-plt.title('Positional Encoding Test')
-plt.show()
-
-
-# Test Scaled Dot Product Attention
-# def test_scaled_dot_product_attention(q, k, v):
-#     temp_out, temp_attn = scaled_dot_product_attention(q, k, v)
-#     print('Attention weights are:')
-#     print(temp_attn)
-#     print('Output is:')
-#     print(temp_out)
-#
-#
-# np.set_printoptions(suppress=True)
-# temp_k = tf.constant([[10, 0, 0],
-#                       [0, 10, 0],
-#                       [0, 0, 10],
-#                       [0, 0, 10]], dtype=tf.float32)
-# temp_v = tf.constant([[1, 0],
-#                       [10, 0],
-#                       [100, 5],
-#                       [1000, 6]], dtype=tf.float32)
-# temp_q = tf.constant([[0, 10, 0]], dtype=tf.float32)
-# test_scaled_dot_product_attention(temp_q, temp_k, temp_v)
-# temp_q = tf.constant([[0, 0, 10]], dtype=tf.float32)
-# test_scaled_dot_product_attention(temp_q, temp_k, temp_v)
-# temp_q = tf.constant([[10, 10, 0]], dtype=tf.float32)
-# test_scaled_dot_product_attention(temp_q, temp_k, temp_v)
-# temp_q = tf.constant([[0, 0, 10], [0, 10, 0], [10, 10, 0]], dtype=tf.float32)
-# test_scaled_dot_product_attention(temp_q, temp_k, temp_v)
-
-
-# Test Multi Head Attention
-# temp_mha = MultiHeadAttention(d_model=512, n_heads=8)
-# y = tf.random.uniform((1, 60, 512))  # (batch_size, encoder_sequence, d_model)
-# out, attn = temp_mha(y, k=y, q=y)
-# print(out.shape, attn.shape)
-
-
-# Point-wise Feed Forward Network
-# sample_ffn = point_wise_feed_forward_network(512, 2048)
-# print(sample_ffn(tf.random.uniform((64, 50, 512))).shape)
-
-
-# Encoder Layer
-# sample_encoder_layer = EncoderLayer(512, 8, 2048)
-# sample_encoder_layer_output = sample_encoder_layer(tf.random.uniform((64, 43, 512)), False)
-# print(sample_encoder_layer_output.shape)
-#
-#
-# # Decoder Layer
-# sample_decoder_layer = DecoderLayer(512, 8, 2048)
-# sample_decoder_layer_output, _, _ = sample_decoder_layer(
-#     tf.random.uniform((64, 50, 512)), sample_encoder_layer_output, False)
-# print(sample_decoder_layer_output.shape)
-#
-#
-# # Encoder
-# sample_encoder = Encoder(
-#     n_layers=2, d_model=512, n_heads=8,
-#     dff=2048, input_vocab_size=8500,
-#     maximum_position_encoding=10000
-# )
-# temp_input = tf.random.uniform((64, 62), dtype=tf.int64, minval=0, maxval=200)
-# sample_encoder_output = sample_encoder(temp_input, training=False)
-# print(sample_encoder_output.shape)
+print(features.shape, pos_encoding.shape)
+encoder_out = encoder(features, pos_encoding)
+print(encoder_out.shape)
